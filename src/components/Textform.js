2869e047ca24267b331c763d4b4ab87faef881f0
aadesh0706 <71577243+aadesh0706@users.noreply.github.com>
2024-01-12 00:22:58 +0530

    Update Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -64,11 +64,12 @@ export default function Textform(props) {
                 {/* <h1>{props.heading}</h1> */}
                 <textarea className='form-control' value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert To Uppercase</button>
-            <button className='btn btn-primary mx-2' onClick={handleLowClick}>Convert To Lowercase</button>
-            <button className='btn btn-primary mx-2' onClick={handleCopyClick}>Copy Text</button>        
-            <button className='btn btn-primary mx-2' onClick={handlePasteClick}>Paste Text</button>
-            <button className='btn btn-primary mx-2' onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopyClick}>Copy Text</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handlePasteClick}>Paste Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+
         </div> 
 
         <div className='container my-3'>
