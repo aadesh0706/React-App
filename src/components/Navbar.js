c2abd498d1564d1d93fc8c1ab0cb348184b57386
aadesh0706 <71577243+aadesh0706@users.noreply.github.com>
2024-01-12 14:40:49 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,8 @@ export default function Navbar(props) {
   
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode}`}>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5588927612636319"
+     crossorigin="anonymous"></script>
      <a className='navbar-brand' href="https://editify.vercel.app/">{props.title}</a>
         <button className='navbar-toggler' type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className='navbar-toggler-icon'></span>
